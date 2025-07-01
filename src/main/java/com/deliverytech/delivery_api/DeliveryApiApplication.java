1ddb39d092af2ec283c524c9f7bdaeea77a123df
reblreis <rebreis88@gmail.com>
2025-07-01 11:45:56 -0300

    feat: adiciona HealthController com endpoints /health e /info e README

--- src/main/java/com/deliverytech/delivery_api/DeliveryApiApplication.java
+++ src/main/java/com/deliverytech/delivery_api/DeliveryApiApplication.java
@@ -3,11 +3,18 @@ package com.deliverytech.delivery_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class DeliveryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void showJavaVersion() {
+    	System.out.println("Java version: " + System.getProperty("java.version"));
 	}
 
 }
